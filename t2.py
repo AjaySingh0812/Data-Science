67d02eb02edc316d7e9251d766a9be57da36768d
Ajay <ajaysingh99064@gmail.com>
2023-07-26 19:52:26 +0530

    for loop

---
+++ t2.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+pensize(2)
+speed('fastest')
+bgcolor('black')
+pencolor('red')
+width(2)
+for i in range(6):
+    lt(60)
+    fd(100)
+    for i in range(6):
+        lt(60)
+        fd(50) 
+        fillcolor('blue')  
+        begin_fill()
+    circle(90,60)             
+hideturtle()
+mainloop()        
